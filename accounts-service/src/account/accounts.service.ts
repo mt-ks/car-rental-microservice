a37102767f0f6fec93a291067e56821d31a5cd2d
MTKS <appledeveloper@monappbilisim.com>
2024-07-13 23:05:24 +0300

    Create accounts service

---
+++ accounts-service/src/account/accounts.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { CreateAccountDto } from './dtos/create-account.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Account } from './schemas/accounts.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AccountsService {
+  constructor(@InjectModel(Account.name) private model: Model<Account>) {}
+
+  async create(createAccountDto: CreateAccountDto) {
+    const isEmailInUse = await this.isEmailInUse(createAccountDto.email);
+    if (isEmailInUse) {
+      throw new BadRequestException('Email already in use');
+    }
+    const account = new this.model(createAccountDto);
+    return account.save();
+  }
+
+  async isEmailInUse(email: string) {
+    const find = await this.model.findOne({ email });
+    if (find) {
+      return true;
+    }
+    return false;
+  }
+
+  update() {}
+}
