209b2cbc39db49b8a49063d64510dc69f2d547d3
MTKS <appledeveloper@monappbilisim.com>
2024-07-14 00:33:56 +0300

    add Accounts module

---
+++ accounts-service/src/account/schemas/account.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Account extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  surname: string;
+
+  @Prop({ default: null })
+  phone_number: string;
+
+  @Prop({ default: false })
+  is_email_verified: boolean;
+
+  @Prop({ default: false })
+  is_phone_verified: boolean;
+
+  @Prop()
+  nationality: string;
+}
+
+export const AccountSchema = SchemaFactory.createForClass(Account);
