49a2b7c9223d2be79b63fc9fef766b6d126a00b0
MTKS <appledeveloper@monappbilisim.com>
2024-07-17 00:45:33 +0300

    add jwt module and other dependencies

--- accounts-service/src/account/accounts.module.ts
+++ accounts-service/src/account/accounts.module.ts
@@ -1,19 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AccountsController } from './accounts.controller';
-import { AccountsService } from './accounts.service';
-import { Account, AccountSchema } from './schemas/account.schema';
+import { AuthController } from './auth.controller';
+import { UsersService } from './users.service';
+import { User, UserSchema } from './schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Permission, PermissionSchema } from './schemas/permission.schema';
+import { PasswordEncryption } from './password-encryption.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PermissionService } from './permission.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Account.name, schema: AccountSchema },
+      { name: User.name, schema: UserSchema },
       { name: Permission.name, schema: PermissionSchema },
     ]),
+    JwtModule.register({
+      global: true,
+      secret: 'myjwtsecret',
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
 
-  controllers: [AccountsController],
-  providers: [AccountsService],
+  controllers: [AuthController],
+  providers: [UsersService, PermissionService, PasswordEncryption, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AccountsModule {}
