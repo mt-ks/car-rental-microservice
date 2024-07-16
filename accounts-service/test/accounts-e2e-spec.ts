5ca087d72c98ab4588367e89c6565dd91a473f78
MTKS <appledeveloper@monappbilisim.com>
2024-07-17 00:46:03 +0300

    update e2e test

--- accounts-service/test/accounts-e2e-spec.ts
+++ accounts-service/test/accounts-e2e-spec.ts
@@ -3,17 +3,18 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AccountsModule } from '../src/account/accounts.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Account, AccountSchema } from '../src/account/schemas/account.schema';
-import { AccountsService } from '../src/account/accounts.service';
+import { User, UserSchema } from '../src/account/schemas/user.schema';
+import { UsersService } from '../src/account/users.service';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import mongoose from 'mongoose';
+import createFakeAccountDto from '../src/account/faker/create-account-dto-fake';
 
 describe('AccountsController (e2e)', () => {
   let app: INestApplication;
-  let accountsService: AccountsService;
+  let accountsService: UsersService;
   let mongoServer: MongoMemoryServer;
-  beforeEach(async () => {
+  beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
     const mongoUri = mongoServer.getUri();
 
@@ -24,26 +25,103 @@ describe('AccountsController (e2e)', () => {
             uri: mongoUri,
           }),
         }),
-        MongooseModule.forFeature([
-          { name: Account.name, schema: AccountSchema },
-        ]),
         AccountsModule,
       ],
     }).compile();
 
-    accountsService = moduleFixture.get<AccountsService>(AccountsService);
+    accountsService = moduleFixture.get<UsersService>(UsersService);
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/api/accounts/signup (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/api/accounts/signup')
-      .expect(200)
-      .expect('signup');
+  it('/api/accounts/create successfuly (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/accounts/create')
+      .send({
+        email: 'test@test.com',
+        password: 'staticpassword',
+        name: 'Mt',
+        surname: 'Ks',
+        nationality: 'TR',
+      });
+
+    expect(response.body).toHaveProperty('status');
+    expect(response.body).toHaveProperty('user');
+    expect(response.body).toHaveProperty('user.email');
+    expect(response.body).toHaveProperty('user.name');
+    expect(response.body).toHaveProperty('user.surname');
+    expect(response.body).toHaveProperty('user.nationality');
+    expect(response.body).not.toHaveProperty('user.password');
+    expect(response.headers).toHaveProperty('authorization');
   });
+
+  it('/api/accounts/create failed (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/accounts/create')
+      .send({
+        email: 'test@test.com',
+        password: 'staticpassword',
+        name: 'Mt',
+        surname: 'Ks',
+        nationality: 'TR',
+      });
+
+    expect(response.body).toHaveProperty('error');
+    expect(response.body).toHaveProperty('message');
+    expect(response.body).toHaveProperty('statusCode');
+  });
+
+  it('/api/accounts/login create account and login successfully & failure (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/accounts/create')
+      .send({
+        email: 'test@test.com',
+        password: 'staticpassword',
+        name: 'Mt',
+        surname: 'Ks',
+        nationality: 'TR',
+      });
+
+    expect(response.body).toHaveProperty('error');
+    expect(response.body).toHaveProperty('message');
+    expect(response.body).toHaveProperty('statusCode');
+
+    const loginSuccess = await request(app.getHttpServer())
+      .post('/api/accounts/login')
+      .send({ email: 'test@test.com', password: 'staticpassword' });
+
+    expect(loginSuccess.body).toHaveProperty('status');
+    expect(loginSuccess.body).toHaveProperty('user');
+    expect(loginSuccess.body).toHaveProperty('user.id');
+    expect(loginSuccess.body).toHaveProperty('user.email');
+    expect(loginSuccess.body).toHaveProperty('user.name');
+    expect(loginSuccess.body).toHaveProperty('user.surname');
+    expect(loginSuccess.body).toHaveProperty('user.nationality');
+
+    const loginFailed = await request(app.getHttpServer())
+      .post('/api/accounts/login')
+      .send({ email: 'test@test.com', password: 'staticpasswordx' });
+
+    expect(loginFailed.statusCode).toEqual(401);
+  });
+
+  it('/api/accounts/info (GET)', async () => {
+    const createAcc = await request(app.getHttpServer())
+      .post('/api/accounts/create')
+      .send(createFakeAccountDto());
+
+    const res = await request(app.getHttpServer())
+      .get('/api/accounts/info')
+      .set('Authorization', `${createAcc.headers.authorization}`);
+
+    expect(res.body).toHaveProperty('status');
+    expect(res.body).toHaveProperty('user');
+    expect(res.body).toHaveProperty('user.id');
+  });
+
   afterAll(async () => {
-    await mongoServer.stop();
     await mongoose.disconnect();
+    await mongoServer.stop();
+    await app.close();
   });
 });
