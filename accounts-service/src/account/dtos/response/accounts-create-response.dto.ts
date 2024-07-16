eadb2b0ba1761966d087836c5e6d2b9696f2032b
MTKS <appledeveloper@monappbilisim.com>
2024-07-17 00:45:53 +0300

    add response dto

---
+++ accounts-service/src/account/dtos/response/accounts-create-response.dto.ts
@@ -0,0 +1,11 @@
+import { Exclude, Expose, Type } from 'class-transformer';
+import { AccountDto } from './account.dto';
+
+export class AccountsCreateResponseDto {
+  @Expose()
+  status: string;
+
+  @Expose()
+  @Type(() => AccountDto)
+  user: AccountDto;
+}
